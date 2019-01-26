2577a493a18a5b4bf4045be0db37073934632c2c
niculae c.i. (cin1g15) <cin1g15@soton.ac.uk>
2019-01-26 20:34:53 +0000

    Add terrain.

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    Rigidbody rb;
+       
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = Vector3.forward * Input.GetAxis("Vertical") * 10
+                    + Vector3.right * Input.GetAxis("Horizontal") * 10;
+    }
+}
