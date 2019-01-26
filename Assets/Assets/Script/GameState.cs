2912022e96d5bafd8f606166c8114b8a39009a8b
stbogdan <bogdan.stoicescu95@gmail.com>
2019-01-26 19:53:30 +0000

    Pause menu transplant in Men 2 scene

---
+++ Assets/Assets/Script/GameState.cs
@@ -0,0 +1,3 @@
+﻿public enum GameState  {
+   Play, Pause, Finale, Ending
+}
---
+++ Assets/Assets/Script/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject pauseOverlay;
+
+    public void Toggle() {
+        if (pauseOverlay.activeInHierarchy) {
+            pauseOverlay.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else {
+            pauseOverlay.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    // Exit to title menu
+    public void Exit () {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(0);
+    }
+ 
+    // Reload current scene
+	public void Restart () {
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+	void LateUpdate () {
+        // Toggle paused state on Esc key
+        if (Input.GetButtonDown("Cancel"))
+            Toggle();
+
+        // Restart if r pressed
+        if (Input.GetButtonDown("Restart"))
+            Restart();
+    }
+}
