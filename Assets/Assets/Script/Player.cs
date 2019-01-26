08ec1fcbefa5f2d9f886860073d51e74dcc2e8a3
Teo G Nistor <teo.g.nistor@gmail.com>
2019-01-26 14:08:27 +0000

    Men

---
+++ Assets/Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        rb.velocity = Vector3.forward * Input.GetAxis("Vertical")
+                    + Vector3.right * Input.GetAxis("Horizontal");
+
+    }
+}
---
+++ Assets/Assets/Script/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        print("Entered " + other.gameObject.name);
+    }
+}
+
+public abstract class Resource {
+    internal abstract float Value { get; }
+}
+
+public class Rock : Resource {
+    internal override float Value {
+        get {
+            return 14;
+        }
+    }
+}
