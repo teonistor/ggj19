af583a5910b502504d87838287500584ed0e2d63
Teo G Nistor <teo.g.nistor@gmail.com>
2019-01-26 16:23:35 +0000

    Player movement input

--- Assets/Assets/Script/Player.cs
+++ Assets/Assets/Script/Player.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour {
-    static readonly Vector3 pickedUpLocalPosition = new Vector3(0f, 1.8f, 0f);
+    static readonly Vector3 pickedUpLocalPosition = new Vector3(0f, 1.9f, 0f);
+
+    const string verticalAxTemplate = "VerticalP{0}",
+                 horizontalAxTemplate = "HorizontalP{0}",
+                 throwAxTemplate = "ThrowP{0}",
+                 myResourceTagTemplate = "Player {0} resource";
+
+    [SerializeField][Range(1,2)] int playerNum;
+
+    //[SerializeField] PlayerNum player;
+    //[SerializeField] LayerMask[] layerMasks;
+    //[SerializeField] Material[] materials;
+
+
+    //[SerializeField] Tribe tribe; // TODO Cristi
+
+    string verticalAx, horizontalAx, throwAx, myResourceTag;
 
     Rigidbody rb;
     Resource pickedUp;
     [SerializeField] Tribe tribe;
 	void Start () {
+        verticalAx = string.Format(verticalAxTemplate, playerNum);
+        horizontalAx = string.Format(horizontalAxTemplate, playerNum);
+        throwAx = string.Format(throwAxTemplate, playerNum);
+        myResourceTag = string.Format(myResourceTagTemplate, playerNum);
+
         rb = GetComponent<Rigidbody>();
 
     }
 	
 	void Update () {
-        rb.velocity = Vector3.forward * Input.GetAxis("Vertical")
-                    + Vector3.right * Input.GetAxis("Horizontal");
+        rb.velocity = Vector3.forward * Input.GetAxis(verticalAx)
+                    + Vector3.right * Input.GetAxis(horizontalAx);
 
-        if (Input.GetButtonDown("Jump") && pickedUp != null) {
+        if (Input.GetButtonDown(throwAx) && pickedUp != null) {
             pickedUp = pickedUp.Throw(transform.forward);
         }
 
     }
+
 
     void OnTriggerEnter (Collider other) {
         pickedUp = other.GetComponent<Resource>();
@@ -33,6 +55,7 @@ public class Player : MonoBehaviour {
         }
     }
 
+
     //function for delivering resources to tribe
     void DeliverResources() {
         if (this.pickedUp != null && this.pickedUp.value > 0) {
@@ -41,13 +64,27 @@ public class Player : MonoBehaviour {
         }
     }
 
-    //function muie teo
-    void Die() {
+
+    void Die () {
         this.pickedUp = null;
         if (tribe.UseLife()) {
             // respawn
-        } else {
+        }
+        else {
             // game over
+        }
+    }
+
+    void OnCollisionEnter (Collision collision) {
+  
+        Resource resource = collision.collider.GetComponent<Resource>();
+        if (resource != null) {
+            if (resource.tag.Equals(Resource.resourceTag)) {
+                pickedUp = resource.PickUp(transform, pickedUpLocalPosition, myResourceTag);
+            } else if (!resource.tag.Equals(myResourceTag)) {
+                // TODO
+                Debug.LogWarning("DEAD");
+            }
         }
     }
 }
--- Assets/Assets/Script/Resource.cs
+++ Assets/Assets/Script/Resource.cs
@@ -5,42 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Resource : MonoBehaviour {
     const float throwVelocity = 5f;
+    internal const string p1ResourceTag = "Player 1 resource",
+                          p2ResourceTag = "Player 2 resource",
+                            resourceTag = "Resource";
 
     Rigidbody rb;
-    Collider cld;
+    //Collider cld;
     int defaultLayer;
     public int value { get; private set; }
 
     void Start () {
         value = 10;
         rb = GetComponent<Rigidbody>();
-        cld = GetComponent<Collider>();
+        //cld = GetComponent<Collider>();
         defaultLayer = gameObject.layer;
     }
 
     internal Resource Throw (Vector3 direction) {
         transform.parent = null;
-        cld.isTrigger = false;
+        //cld.isTrigger = false;
+        //rb.useGravity = true;
         rb.isKinematic = false;
-        rb.velocity = Vector3.RotateTowards(direction, Vector3.up, Mathf.PI / 4f, float.PositiveInfinity)
+        rb.velocity = Vector3.RotateTowards(direction, Vector3.up, Mathf.PI / 6f, float.PositiveInfinity)
             . normalized
             * throwVelocity;
 
         return null; // Asigned in Player
     }
 
-    //internal Resource PickUp (Vector3 holdingPosition) {
-    //    rb.isKinematic = 
-    //    transform.position = holdingPosition;
+    internal Resource PickUp (Transform newParent, Vector3 newLocalPosition, string newTag) {
+        //rb.useGravity = false;
+        transform.parent = newParent;
+        transform.localPosition = newLocalPosition;
+        tag = newTag;
 
-    //    return this; // Asigned in Player
-    //}
+        return this; // Asigned in Player
+    }
 
     void OnCollisionEnter (Collision collision) {
         if (collision.collider.tag == "Ground") {
             rb.isKinematic = true;
-            cld.isTrigger = true;
-            gameObject.layer = defaultLayer;
+            //cld.isTrigger = true;
+            //gameObject.layer = defaultLayer;
+            gameObject.tag = resourceTag;
         }
     }
 
