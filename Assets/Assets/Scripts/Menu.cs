8b0c89e9ce173259806a0c2afb41ea4aadcb9329
stbogdan <bogdan.stoicescu95@gmail.com>
2019-01-26 14:50:13 +0000

    Added main menu (taken from last year)

---
+++ Assets/Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField] private GameObject[] titleScreen, rulesScreen;
+
+    private void Awake () {
+        // In case we return to title screen from paused game
+        Time.timeScale = 1f;
+        activateAll(titleScreen);
+    }
+
+    public void Play () {
+        deactivateAll(titleScreen);
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Rules () {
+        deactivateAll(titleScreen);
+        activateAll(rulesScreen);
+    }
+
+    public void Quit () {
+        Application.Quit();
+    }
+
+    public void Back () {
+        deactivateAll(rulesScreen);
+        activateAll(titleScreen);
+    }
+
+    private void activateAll (GameObject[] objs) {
+        foreach (GameObject o in objs)
+            o.SetActive(true);
+    }
+
+    private void deactivateAll (GameObject[] objs) {
+        foreach (GameObject o in objs)
+            o.SetActive(false);
+    }
+}
