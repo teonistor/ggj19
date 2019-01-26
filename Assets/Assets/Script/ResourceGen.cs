9f6e2885f7ae35075fb18cd07f224f965fc143b9
Teo G Nistor <teo.g.nistor@gmail.com>
2019-01-26 23:25:19 +0000

    Resource spawner

---
+++ Assets/Assets/Script/ResourceGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGen : MonoBehaviour {
+
+    [SerializeField] GameObject[] resources;
+
+    IEnumerator Start () {
+        for (WaitForSeconds wait = new WaitForSeconds(Random.Range(2f, 10f))
+                ; ;         wait = new WaitForSeconds(Random.Range(20f, 30f))) {
+            yield return wait;
+
+            Instantiate(RandomResource(), transform, false).transform.parent = null;
+        }
+    }
+
+    GameObject RandomResource () {
+        return resources[Random.Range(0, resources.Length)];
+    }
+}
