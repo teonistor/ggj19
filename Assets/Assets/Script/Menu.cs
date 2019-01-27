e4a956ee6670c5bed802aa95bf11b4b1c7849459
Teo G Nistor <teo.g.nistor@gmail.com>
2019-01-27 14:48:29 +0000

    fix main menu

--- Assets/Assets/Script/Menu.cs
+++ Assets/Assets/Script/Menu.cs
@@ -14,6 +14,7 @@ public class Menu : MonoBehaviour {
     public void Play () {
         deactivateAll(titleScreen);
         SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
     }
 
     public void Rules () {
