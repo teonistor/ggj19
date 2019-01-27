57ad2b8026db5e1251607d7c41a5b7bf897d2250
stbogdan <bogdan.stoicescu95@gmail.com>
2019-01-27 14:45:58 +0000

    Added ragdolls (again)

--- Assets/Assets/Script/Player.cs
+++ Assets/Assets/Script/Player.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     static readonly float velocityMagnitudeSafe = 1.5f;
 
     static readonly Vector3 pickedUpLocalPosition = new Vector3(0f, 1.26667f, 0f);
@@ -13,21 +14,23 @@ public class Player : MonoBehaviour {
                  throwAxTemplate = "ThrowP{0}",
                  myResourceTagTemplate = "Player {0} resource";
 
-    [SerializeField][Range(1,2)] int playerNum;
+    [SerializeField] [Range(1, 2)] int playerNum;
     [SerializeField] Tribe tribe;
-  
+
 
     public string verticalAx { get; private set; }
     public string horizontalAx { get; private set; }
     public string throwAx { get; private set; }
     public string myResourceTag { get; private set; }
+    public GameObject teamRagdoll;
 
     Rigidbody rb;
     Resource pickedUp;
     SimpleCharacterControl scc;
     Vector3 spawnPoint;
 
-	void Start () {
+    void Start()
+    {
         verticalAx = string.Format(verticalAxTemplate, playerNum);
         horizontalAx = string.Format(horizontalAxTemplate, playerNum);
         throwAx = string.Format(throwAxTemplate, playerNum);
@@ -37,22 +40,30 @@ public class Player : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         scc = GetComponent<SimpleCharacterControl>();
     }
-	
-	void Update () {
-        if (Input.GetButtonDown(throwAx) && pickedUp != null) {
+
+    void Update()
+    {
+        if (Input.GetButtonDown(throwAx) && pickedUp != null)
+        {
             pickedUp = pickedUp.Throw(transform.forward);
         }
     }
 
 
-    internal void Die () {
+    internal void Die()
+    {
         pickedUp = null;
-        if (tribe.UseLife()) {
+        if (tribe.UseLife())
+        {
             // respawn
             scc.enabled = false;
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+            Instantiate(teamRagdoll, transform.position, transform.rotation);
             StartCoroutine(Respawn(spawnPoint));
         }
-        else {
+        else
+        {
             // game over
             // this will notify the GameController that player is kill
             Destroy(gameObject);
@@ -64,6 +75,8 @@ public class Player : MonoBehaviour {
         //Wait a bit before transfering the player to spawn
         yield return new WaitForSeconds(2);
         transform.position = spawnResetPoint;
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
         scc.enabled = true;
     }
 
@@ -72,33 +85,41 @@ public class Player : MonoBehaviour {
     /// it means it was thrown by the other player and hit us
     /// </summary>d
     /// <param name="collision"></param>
-    void OnCollisionEnter (Collision collision) {
+    void OnCollisionEnter(Collision collision)
+    {
         Resource resource = collision.collider.GetComponent<Resource>();
-        if (resource != null) {                
+        if (resource != null)
+        {
             Rigidbody rbObject = resource.GetComponent<Rigidbody>();
-            if (resource.tag.Equals(Resource.resourceTag) && rbObject.velocity.magnitude < velocityMagnitudeSafe) {
-                if (pickedUp == null) {
+            if (resource.tag.Equals(Resource.resourceTag) && rbObject.velocity.magnitude < velocityMagnitudeSafe)
+            {
+                if (pickedUp == null)
+                {
                     pickedUp = resource.PickUp(transform, pickedUpLocalPosition, myResourceTag);
                 }
-            } else if (!resource.tag.Equals(myResourceTag)) {
+            }
+            else if (!resource.tag.Equals(myResourceTag))
+            {
                 Die();
                 Debug.LogWarning(gameObject.name + " hit by rock");
             }
         }
     }
 
-    void OnTriggerEnter (Collider other) {
+    void OnTriggerEnter(Collider other)
+    {
         Tribe tribe = other.GetComponent<Tribe>();
-        if (tribe != null) {
+        if (tribe != null)
+        {
             //if (tribe.Equals(this.tribe)) {
             //    if (pickedUp != null) {
             //        tribe.AddResources(pickedUp.value);
             //        Destroy(pickedUp.gameObject);
-                    pickedUp = null;
-                }
-            //} else {
-            //    print("What are you doing in the adversary's tribe...");
-            //}
+            pickedUp = null;
+        }
+        //} else {
+        //    print("What are you doing in the adversary's tribe...");
+        //}
         //}
     }
 }
